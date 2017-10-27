165ef3df6ab58b6da999a2df867ce59739fce10a
Eric Holman <eric.l.holman.1107@icloud.com>
2017-10-26 19:19:21 -0600

    modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -28,10 +37,4 @@ end
 
 puts "9 portfolio items creates"
 
-3.times do |topic|
-  Topic.create!(
-    title: "Topic #{topic}"
-  )
-end
 
-puts "3 Topics created"
